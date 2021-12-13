26ee56a793dd05a0d09e1dad242b01330937826a
AskJe18 <AskJe18@student.sdu.dk>
2021-12-13 15:08:04 +0100

    more A3

--- Assignment3/test.py
+++ Assignment3/test.py
@@ -1,19 +1,27 @@
 import numpy as np
+import scipy.stats
 
-v = np.array([-1,1])
-print(v)
+# v = np.array([-1,1])
+# print(v)
 
-matrix = np.array([[2, 0.8], [0.8, 0.6]])
-print(matrix)
+# matrix = np.array([[2, 0.8], [0.8, 0.6]])
+# print(matrix)
 
-multi_1 = np.dot(matrix, v)
-print("1st multiplication: ",multi_1)
+# multi_1 = np.dot(matrix, v)
+# print("1st multiplication: ",multi_1)
 
-multi_2 = np.dot(matrix, multi_1)
-print("2nd multiplication: ",multi_2)
+# multi_2 = np.dot(matrix, multi_1)
+# print("2nd multiplication: ",multi_2)
 
-multi_3 = np.dot(matrix, multi_2)
-print("3rd multiplication: ",multi_3)
+# multi_3 = np.dot(matrix, multi_2)
+# print("3rd multiplication: ",multi_3)
 
-multi_4 = np.dot(matrix, multi_3)
-print("4th multiplication: ", multi_4)
+# multi_4 = np.dot(matrix, multi_3)
+# print("4th multiplication: ", multi_4)
+
+gamma = 0.95
+fac1 = scipy.stats.norm.ppf((1-gamma)/2, 0, 1)  
+fac2 = scipy.stats.norm.ppf((1-gamma)/2 + gamma, 0, 1)
+
+print(fac1)
+print(fac2)
